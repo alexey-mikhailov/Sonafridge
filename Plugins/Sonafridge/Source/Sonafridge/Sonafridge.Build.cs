80882b58a255452c34600ee3621366eb6ce3ffde
alexey.mikhailov <>
2022-06-29 23:03:33 +0300

    Synthesis is tempporary added as a dependency

--- Plugins/Sonafridge/Source/Sonafridge/Sonafridge.Build.cs
+++ Plugins/Sonafridge/Source/Sonafridge/Sonafridge.Build.cs
@@ -4,52 +4,55 @@ using UnrealBuildTool;
 
 public class Sonafridge : ModuleRules
 {
-	public Sonafridge(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"InputCore",
-				"Engine",
+    public Sonafridge(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add public include paths required here ...
+            }
+        );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add other private include paths required here ...
+            }
+        );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new[]
+            {
+                "Core"
+                // ... add other public dependencies that you statically link with here ...
+            }
+        );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new[]
+            {
+                "InputCore",
+                "Engine",
                 "CoreUObject",
-				"Slate",
-				"SlateCore",
-				"UMG"
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+                "Slate",
+                "SlateCore",
+                "Synthesis",
+                "UMG"
+                // ... add private dependencies that you statically link with here ...	
+            }
+        );
+
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+                // ... add any modules that your module loads dynamically here ...
+            }
+        );
+    }
 }
